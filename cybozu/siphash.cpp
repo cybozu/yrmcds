ea8daf4cbac702a8a3125aadfc41f2a6abb2f5e5
MITSUNARI Shigeo <herumi@nifty.com>
2014-01-09 14:00:58 +0900

    avoid bad alignment for int type

--- cybozu/siphash.cpp
+++ cybozu/siphash.cpp
@@ -33,6 +33,7 @@
 
 #include "siphash.hpp"
 #include <endian.h>
+#include <memory.h>
 
 #define ROTATE(x, b) (std::uint64_t)( ((x) << (b)) | ( (x) >> (64 - (b))) )
 
@@ -62,14 +63,16 @@ uint64_t static_k1;
 namespace cybozu {
 
 void siphash24_seed(const char key[16]) {
-    const uint64_t *_key = (const uint64_t *)key;
-    static_k0 = le64toh(_key[0]);
-    static_k1 = le64toh(_key[1]);
+    uint64_t t;
+    memcpy(&t, key, sizeof(t));
+    static_k0 = le64toh(t);
+    memcpy(&t, key + 8, sizeof(t));
+    static_k1 = le64toh(t);
 }
 
 uint64_t siphash24(const void *src, std::size_t src_sz) {
     uint64_t b = (uint64_t)src_sz << 56;
-    const uint64_t *in = (const uint64_t*)src;
+    const uint8_t *m = (const uint8_t*)src;
 
     uint64_t v0 = static_k0 ^ 0x736f6d6570736575ULL;
     uint64_t v1 = static_k1 ^ 0x646f72616e646f6dULL;
@@ -77,14 +80,15 @@ uint64_t siphash24(const void *src, std::size_t src_sz) {
     uint64_t v3 = static_k1 ^ 0x7465646279746573ULL;
 
     while (src_sz >= 8) {
-        uint64_t mi = le64toh(*in);
-        in += 1; src_sz -= 8;
+        uint64_t mi;
+        memcpy(&mi, m, sizeof(mi));
+        mi = le64toh(mi);
+        m += 8; src_sz -= 8;
         v3 ^= mi;
         DOUBLE_ROUND(v0,v1,v2,v3);
         v0 ^= mi;
     }
 
-    const uint8_t *m = (const uint8_t *)in;
     switch (src_sz) {
     case 7: b |= uint64_t(m[6]) << 48;
     case 6: b |= uint64_t(m[5]) << 40;
