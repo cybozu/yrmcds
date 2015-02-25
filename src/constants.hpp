36787fe383a8bea58cfcce1c296b8e25b3b496ae
ymmt <ymmt@cybozu.com>
2015-02-25 09:29:51 +0900

    Bump version to 1.1.1

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,5 +1,5 @@
 // Constants used in yrmcds.
-// (C) 2013, 2014 Cybozu.
+// (C) 2013-2015 Cybozu.
 
 #ifndef YRMCDS_CONSTANTS_HPP
 #define YRMCDS_CONSTANTS_HPP
@@ -31,7 +31,7 @@ const std::size_t   MAX_REQUEST_LENGTH  = 30 << 20; // 30 MiB
 const int           MAX_SLAVES          = 5;
 const int           MAX_CONSECUTIVE_GCS = 3;
 
-const char          VERSION[] = "yrmcds version 1.1.0";
+const char          VERSION[] = "yrmcds version 1.1.1";
 
 } // namespace yrmcds
 
