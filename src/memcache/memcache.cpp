3b5237499458d9694f760827895f646e2660de79
ymmt <ymmt@cybozu.com>
2015-02-25 09:28:03 +0900

    Let "stat settings" answer "lock_memory" setting.

--- src/memcache/memcache.cpp
+++ src/memcache/memcache.cpp
@@ -592,6 +592,8 @@ void text_response::stats_settings() {
     os << "STAT locking on" << CRLF;
     os << "STAT secure_erase "
        << (g_config.secure_erase() ? "on" : "off") << CRLF;
+    os << "STAT lock_memory "
+       << (g_config.lock_memory() ? "on" : "off") << CRLF;
     os << "STAT tmp_dir " << g_config.tempdir() << CRLF;
     os << "STAT buckets " << g_config.buckets() << CRLF;
     os << "STAT item_size_max " << g_config.max_data_size() << CRLF;
@@ -1045,6 +1047,7 @@ binary_response::stats_settings() {
     send_stat("cas_enabled", "on");
     send_stat("locking", "on");
     send_stat("secure_erase", g_config.secure_erase() ? "on" : "off");
+    send_stat("lock_memory", g_config.lock_memory() ? "on" : "off");
     send_stat("tmp_dir", g_config.tempdir());
     send_stat("buckets", std::to_string(g_config.buckets()));
     send_stat("item_size_max", std::to_string(g_config.max_data_size()));
