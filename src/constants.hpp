922d0621539813db49845567e8dc8e59cfaeb360
ymmt <ymmt@cybozu.com>
2015-03-23 20:48:44 +0900

    Bump version to 1.1.3

--- src/constants.hpp
+++ src/constants.hpp
@@ -31,7 +31,7 @@ const std::size_t   MAX_REQUEST_LENGTH  = 30 << 20; // 30 MiB
 const int           MAX_SLAVES          = 5;
 const int           MAX_CONSECUTIVE_GCS = 3;
 
-const char          VERSION[] = "yrmcds version 1.1.2";
+const char          VERSION[] = "yrmcds version 1.1.3";
 
 } // namespace yrmcds
 
