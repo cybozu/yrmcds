8f20b8dda09da853fc04a6270f6915b577cab2da
ymmt2005 <ymmt2005@gmail.com>
2016-10-03 14:00:10 +0900

    Bump version to 1.1.7

--- src/constants.hpp
+++ src/constants.hpp
@@ -33,7 +33,7 @@ const std::size_t   MAX_REQUEST_LENGTH  = 30 << 20; // 30 MiB
 const int           MAX_SLAVES          = 5;
 const int           MAX_CONSECUTIVE_GCS = 3;
 
-const char          VERSION[] = "yrmcds version 1.1.6";
+const char          VERSION[] = "yrmcds version 1.1.7";
 
 } // namespace yrmcds
 
